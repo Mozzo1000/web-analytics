46bd8e8738e8ea09160e529312e85ccdd51eaaae
Andreas Backström <mozzo242@gmail.com>
2023-07-13 19:47:52 +0200

    Make report more useful

--- src/web_analytics/report.py
+++ src/web_analytics/report.py
@@ -1,21 +1,38 @@
 from jinja2 import Environment, FileSystemLoader
+import matplotlib.pyplot as plt
+import base64
+from io import BytesIO
+import pandas
 
 def generate_report(cur, con):
     environment = Environment(loader=FileSystemLoader("templates/"))
     template = environment.get_template("report.html")
     filename = "results.html"
     urls = []
-    status = []
     cur.execute("SELECT url FROM websites")
     rows = cur.fetchall()
     for row in rows:
         urls.append(row[0])
+    
+    cur.execute("SELECT url, title, category, status FROM websites")
+    all_data = cur.fetchall()
 
-    cur.execute("SELECT status, COUNT(status) FROM websites GROUP BY status")
-    rows = cur.fetchall()
-    for row in rows:
-        status.append(row)
+
+    sql = "SELECT status, COUNT(status) as c_status FROM websites GROUP BY status"
+    status_data = pandas.read_sql(sql, con)
+
+    fig, ax = plt.subplots()
+    bar_container = ax.bar(status_data.status, status_data.c_status, color=['blue', 'red', 'green'])
+    plt.title("Status codes")
+
+    plt.xlabel("HTTP status codes")
+    plt.ylabel("Occurrence of code")
+    ax.bar_label(bar_container)
+
+    tmpfile = BytesIO()
+    fig.savefig(tmpfile, format='png')
+    encoded = base64.b64encode(tmpfile.getvalue()).decode('utf-8')
 
     with open(filename, mode="w", encoding="utf-8") as results:
-        results.write(template.render({"url": urls, "status": status}))
+        results.write(template.render({"all_data": all_data, "url": urls, "image_1": encoded}))
         print(f"wrote {filename}")
